7b872fb0f3d6335465a75f5267cd8894e228f7e0
MiladGhamati <miladmou@yahoo.com>
2024-12-14 23:40:45 +0330

    Add initial project structure and dependencies

---
+++ pages/breeds/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">Dog Breeds Page</h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">Dog Breeds Gallery</h1>
+  </div>
+</template>
